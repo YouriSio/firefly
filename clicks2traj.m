d894d196f8d5e96516a10848474f85d9a51cda90
BramBruin <33868747+TheFappert@users.noreply.github.com>
2018-10-10 12:48:35 +0200

    Cleaning/optimizing
    Clean up of code and comments, optimized a for loop by defining a size beforehand. For loop was changing size of vector every iteration.

--- clicks2traj.m
+++ clicks2traj.m
@@ -1,53 +1,55 @@
 %% Define the trajectory by clicking (x,y) coordinates
 %
 % Using Matlab built in function getpts.m it starts by asking the user to 
-% introduce some points on the image, which for convinience if the image of
+% introduce some points on the image, which for convenience if the image of
 % the soccer field: these points can directly be used by the simulation with
 % the soccer field environment. Click once for introducing points and twice 
 % for the final point. This defines the x, and y coordinates. The z and psi 
 % coordinates as well as the times at which the drone should reach each way 
 % point are defined automatically or can be customized by the user. Many control 
 % options allow the user to define this as well as using previously clicked points.
-% After this, it runs the initscript
+% After this, it runs the initscript.
 
-%% options  
-clear all, close all
+%% Clean up
+clear 
+close all
+clc
 
-optconstantspeed = 1;  % 1- approx constant speed, 2 - customize times between way points
-speed = 1;             % if optconsspeed == 1 this is the velocity
+%% Options
+option.constantspeed = true;   % Defines if the drone will follow a constant speed
+option.constantheight = true;  % Defines if the drone will follow a constant height
+option.constantyaw = true;     % Defines if the drone will have a constan yaw
+option.loadwaypoints = false;  % Defines if the trajectory will be loaded from waypoints.mat
+option.savewaypoints = true;   % Defines if the trajectory will be saved to waypoints.mat
+option.saveT = true;           % Defines if the trajectory will be saved to T.mat
+option.plot = true;            % Defines if the resulting trajectory will be plotted
 
-optconstanheight = 2;  % 1- height is constant, 2 - customize height
-height = 1;            % if optconstanheight = 1; this is the height
+%% Constants
+speed = 1;                          % Will be used if constantspeed is true
+height = 1;                         % WIll be used if constantheight is true
+centersoccerfieldpic = [346 281];   % Center of the soccerfield image in pixels
+lengthsoccerfield = [8 10];         % Length of the soccerfield in meters
+rho = 0.001;                        % Weighting factor for control input
+m = 4;                              % Mass of the drone, needs to be larger than 2
+tau = 0.1;                          % Input sampling period
+N12 = 10;                           % Ratio of input sampling period over output sampling period
 
-optconstantyaw    = 2; % 1- yaw is zero, 2 - customize yaw
-
-optloadwaypoints = 2;  % 1- load previously obtained way points px, py in waypoints.mat
-optsavewaypoints = 1;  % 1- save the new way points in waypoints.mat, 2 do not save
-
-optsaveT = 1;          % 1- save the trajectory to T.mat, 2 do not save
-
-optplot = 1;           % 1- plot resulting trajectory, 2 - do not plot
-% -------------------------------------------------------------------------
-
-% -------------------------------------------------------------------------
-%% obtain the way points
-if optloadwaypoints == 1
+%% Obtain the way points
+if option.loadwaypoints == true
     load waypoints
 else
     imshow './_Aux/soccerfieldpic.jpg'
     [xi,yi] = getpts;
-    centersoccerfieldpic = [346 281]; % pixels
-    lengthsoccerfield = [8 10]; %meters
-    p = [ -lengthsoccerfield(1)/2*(yi-centersoccerfieldpic(2))/centersoccerfieldpic(2) lengthsoccerfield(2)/2*(xi-centersoccerfieldpic(1))/centersoccerfieldpic(2)]; % centerin, scaling, and siwtch axis 
+    p = [ -lengthsoccerfield(1)/2*(yi-centersoccerfieldpic(2))/centersoccerfieldpic(2) lengthsoccerfield(2)/2*(xi-centersoccerfieldpic(1))/centersoccerfieldpic(2)]; 
     px = p(:,1);
     py = p(:,2);
     
-    if optsavewaypoints == 1
+    if option.savewaypoints == true
         save waypoints px py
     end
 end
 
-if optconstanheight == 1
+if option.constantheight == true
     pz = -height*ones(size(px)); % -1 due to convention
 else
     pz = -ones(size(px));
@@ -55,10 +57,11 @@ else
 end
 
 N = length(px);
+Tp = zeros(1,N);
 Tp(1) = 0;
 P = [px';py';pz'];
 
-if optconstantspeed == 1
+if option.constantspeed == true
     epsilon = 0.01;
     for i = 2:N
         % if two points are two close, spend epsilon sec
@@ -69,7 +72,7 @@ else
     Tp = 0:N-1; % customize here
 end
 
-if optconstantyaw == 1
+if option.constantyaw == true
     psi = zeros(1,N);
 else
     psi = zeros(1,N);
@@ -77,29 +80,17 @@ else
     psi(4) = -pi/4;
     psi(5) = pi/4;
 end
-% -------------------------------------------------------------------------
 
-% -------------------------------------------------------------------------
-%% from the way points, generate trajectory
-pth  = 3;
-rho = 0.001;
-alpha = 1;
-m = 4; % larger than 2
-N12 = 10;
-tau = 0.1;
+%% Generate trajectory from the way points
 X0 = [P(:,1) speed*(P(:,2)-P(:,1))/Tp(2) zeros(3,m-2)];
 [xix,xiy,xiz]  = lqrtrajgeneration(P,Tp,X0,rho,tau,N12);
 
 Ppsi = [psi;zeros(size(psi));zeros(size(psi))];
 PSI0 = [Ppsi(:,1) (Ppsi(:,2)-Ppsi(:,1))/Tp(2) zeros(3,m-2)];
 [xipsi,~,~]  = lqrtrajgeneration(Ppsi,Tp,PSI0,rho,tau,N12);
-% -------------------------------------------------------------------------
 
-
-
-% -------------------------------------------------------------------------
-%% plot 
-if optplot == 1
+%% Plot
+if option.plot == true
     plot3(P(1,:),P(2,:),P(3,:))
     hold on
     plot3(xix(1,:),xiy(1,:),xiz(1,:))
@@ -117,12 +108,7 @@ if optplot == 1
 end
 delta = 0.1;
 axis( [-lengthsoccerfield(1)/2+delta lengthsoccerfield(1)/2+delta -lengthsoccerfield(2)/2+delta lengthsoccerfield(2)/2+delta -3 +delta])
-% -------------------------------------------------------------------------
-
-
-% -------------------------------------------------------------------------
-%% T format for simulation and run initscript
-tau    = 0.01;    
+%% T format for simulation and run initscript  
 T.X    = xix(1,:);             % X  reference
 T.Y    = xiy(1,:);             % Y  reference
 T.Z    = xiz(1,:);             % Z  reference
@@ -138,10 +124,8 @@ T.APSI = xipsi(3,:);           % yaw acceleration reference
 T.time = (1:size(T.X,2))*tau;  % discrete-time
 T.period = tau;                % sampling period
 
-if optsaveT == 1
+if option.saveT == true
     save T T
 end
 
 initscript
-% -------------------------------------------------------------------------
-
